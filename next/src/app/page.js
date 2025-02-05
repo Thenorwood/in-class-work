d96817695294d7ea759a7ad3544f140afd9335b4
Bryn Norwood <Bryn.Norwood@nscc.ca>
2025-02-05 18:44:14 -0400

    push commit

---
+++ next/src/app/page.js
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+
+
+export default function Home = () => {
+    return (
+        <div className="grid frid-rows-[20px_1fr_20px] items-center justify-items-center">
+
+            <header>
+                <nav>
+                    <a href="">Home</a>
+                    <a href="">About</a>
+                </nav>
+
+            </header>
+
+            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-">
+                <hi>About</hi>
+                <h2>Journey to awesoemness. webapp page</h2>
+            </main>
+            
+            <footer className='row-start-3 flex gap-6 flex-wrap items-center justify-items-center'>
+                <h3>Emerging Technologies</h3>
+                <h3>Delano Marques</h3>
+                <h3>February 5,. 2025</h3>
+            </footer>
+
+
+        </div>
+        
+    )
+}
